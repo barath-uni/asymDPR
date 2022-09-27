a9770f6c4ee3196e339aa77303746266b8bd25fc
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-09-27 22:50:25 +0200

    changes

--- new_metric.py
+++ new_metric.py
@@ -3,29 +3,22 @@ import numpy as np
 import string
 # Need  a metric calculation function for NDCG
 def calculate_ndcg(gold_passages, doc_texts):
-    last_id = -1
-    den_count = 0
-    total_rel = 0
-    for id, row in enumerate(doc_texts):
-        if last_id != id:
-            if last_id != -1:
-                total_rel += rel
-                den_count += 1
-            id_count = 1
-            rel = 0
-        else:
-            id_count += 1
-        logging.info(gold_passages[id])
-        logging.info(row)
-        if gold_passages[id].strip().lower().translate(str.maketrans("", "", string.punctuation)) == row.strip().lower().translate(str.maketrans("", "", string.punctuation)):
-            rel += 1 / np.log2(id_count + 1)
-        last_id = id
-
-    total_rel += rel
-    den_count += 1
-
-    logging.info("NDCG METRIC", total_rel/den_count)
-    return total_rel/den_count
+    logging.info(len(doc_texts))
+    logging.info(len(doc_texts[0]))
+    relevance_list = np.zeros((len(gold_passages), len(doc_texts[0])))
+    for i, (docs, truth) in enumerate(zip(doc_texts, gold_passages)):
+        for j, d in enumerate(docs):    
+            if d.strip().lower().translate(
+                str.maketrans("", "", string.punctuation)
+            ) == truth.strip().lower().translate(
+                str.maketrans("", "", string.punctuation)
+            ):
+                relevance_list[i, j] = 1
+                break
+    logging.info(relevance_list)
+    return 0.0
+    # logging.info("NDCG METRIC", total_rel/den_count)
+    # return total_rel/den_count
 
 def calculate_recall_100(gold_passages, doc_texts):
     act_set = set(gold_passages)
