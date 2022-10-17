f0534944ff4246a9a12d4351a6e0cdbd3a32ab34
baradwaj varadharajan <baradwaj.varadharajan@triply.cc>
2022-10-17 14:29:32 +0200

    changing eval model

--- evaluate_model.py
+++ evaluate_model.py
@@ -16,42 +16,29 @@ transformers_logger.setLevel(logging.WARNING)
 logging.info("Loading doc data...")
 # Read the data from the CSV :TODO Run the modify_dataset.py before running this file
 
-train_data = pd.read_csv(f"data/data_{dataset_dir}/train.tsv", sep="\t")
-eval_data = pd.read_csv(f"data/data_{dataset_dir}/dev.tsv", sep="\t")
+# Path to the trained model
 model_type = "custom"
-model_name = ""
+# Generating index only for the DPR for now. Can switch later for other research questions
+model_name = "output/bert-base-uncased"
+
+# Path to the Wikipedia passage collection
+index_path = "data/data_dataset/corpus_to_index.tsv"
+
 model_args = RetrievalArgs()
-
-# Training parameters
-model_args.num_train_epochs = 1
-model_args.train_batch_size = 40
-model_args.learning_rate = 1e-5
-model_args.max_seq_length = 256
-
-# Evaluation parameters
-model_args.retrieve_n_docs = 100
-model_args.eval_batch_size = 40
-model_args.retrieval_batch_size = 10
-model_args.evaluate_during_training = False
-model_args.evaluate_during_training_verbose = True
-# model_args.evaluate_during_training_steps = 200
+model_args.output_dir = f"output/{model_name}"
 model_args.include_title = False
-# Model tracking
-# model_args.wandb_project = "Dense retrieval with Simple Transformers"
-model_args.save_model_every_epoch = False
-model_args.save_eval_checkpoints = False
-model_args.save_steps = -1
-model_args.save_best_model = True
-model_args.overwrite_output_dir = True
-context_name = "bert-base-uncased"
-
+# Loading the model automatically builds the index
 model = RetrievalModel(
     model_type=model_type,
     model_name=model_name,
-    context_encoder_name=context_name,
-    query_encoder_name="bert-base-uncased",
     args=model_args,
+    prediction_passages=index_path,
     query_static_embeddings=True
 )
 
-model.eval_model(eval_data, top_k_values=[1, 2, 3, 5, 10, 20, 100], output_dir = f"output/bert-base-uncased_static", ndcg=calculate_ndcg, recall_100=calculate_recall_100)
+# Ideally we have to use an unseen data for evaluation (Possibly left out from eval.json to check the performance)
+eval_data = pd.read_csv(f"data/data_dataset/dev_waa.tsv", sep="\t")
+
+
+results, *_ = model.eval_model(eval_data, top_k_values=[1, 2, 3, 5, 10, 20, 100], output_dir = f"output/bert-base-uncased_static", ndcg=calculate_ndcg, recall_100=calculate_recall_100)
+logging.info(results)
